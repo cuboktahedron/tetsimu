4a74ea4c0a78d084b1dd50ac8a3769a3903fe826
cuboktahedron <cuboktahedron@gmail.com>
2015-05-06 09:10:11 +0900

    ネクスト・ホールドの各タイプ別に共通化したコンポーネントを使用するように変更する

--- js/components/hold-edit.jsx
+++ js/components/hold-edit.jsx
@@ -28,122 +28,7 @@ var HoldEditPanel = React.createClass({
     C.HoldEditAction.SetHold();
   },
 
-  // TODO: render各モードで共通化できないか？
   render: function() {
-    switch (this.state.type) {
-      case C.CellType.None:
-        structure = <table className="hold-none" key="none" />
-        break;
-
-      case C.CellType.I:
-        structure =
-          <table className="notice-i" key="i">
-            <tr>
-              <td className="notice-cell type-i" />
-              <td className="notice-cell type-i" />
-              <td className="notice-cell type-i" />
-              <td className="notice-cell type-i" />
-            </tr>
-          </table>
-        break;
-
-      case C.CellType.J:
-        structure =
-          <table className="notice-j" key="j">
-            <tr>
-              <td className="notice-cell type-j" />
-              <td className="notice-cell" />
-              <td className="notice-cell" />
-            </tr>
-            <tr>
-              <td className="notice-cell type-j" />
-              <td className="notice-cell type-j" />
-              <td className="notice-cell type-j" />
-            </tr>
-          </table>
-        break;
-
-      case C.CellType.L:
-        structure =
-          <table className="notice-l" key="l">
-            <tr>
-              <td className="notice-cell" />
-              <td className="notice-cell" />
-              <td className="notice-cell type-l" />
-            </tr>
-            <tr>
-              <td className="notice-cell type-l" />
-              <td className="notice-cell type-l" />
-              <td className="notice-cell type-l" />
-            </tr>
-          </table>
-        break;
-
-      case C.CellType.O:
-        structure =
-          <table className="notice-o" key="o">
-            <tr>
-              <td className="notice-cell type-o" />
-              <td className="notice-cell type-o" />
-            </tr>
-            <tr>
-              <td className="notice-cell type-o" />
-              <td className="notice-cell type-o" />
-            </tr>
-          </table>
-        break;
-
-      case C.CellType.S:
-        structure =
-          <table className="notice-s" key="s">
-            <tr>
-              <td className="notice-cell" />
-              <td className="notice-cell type-s" />
-              <td className="notice-cell type-s" />
-            </tr>
-            <tr>
-              <td className="notice-cell type-s" />
-              <td className="notice-cell type-s" />
-              <td className="notice-cell" />
-            </tr>
-          </table>
-        break;
-
-      case C.CellType.T:
-        structure =
-          <table className="notice-t" key="t">
-            <tr>
-              <td className="notice-cell" />
-              <td className="notice-cell type-t" />
-              <td className="notice-cell" />
-            </tr>
-            <tr>
-              <td className="notice-cell type-t" />
-              <td className="notice-cell type-t" />
-              <td className="notice-cell type-t" />
-            </tr>
-          </table>
-        break;
-
-      case C.CellType.Z:
-        structure =
-          <table className="notice-z" key="z">
-            <tr>
-              <td className="notice-cell type-z" />
-              <td className="notice-cell type-z" />
-              <td className="notice-cell" />
-            </tr>
-            <tr>
-              <td className="notice-cell" />
-              <td className="notice-cell type-z" />
-              <td className="notice-cell type-z" />
-            </tr>
-          </table>
-        break;
-      default:
-        throw new Error('invalid type(' + this.props.type + ')');
-      }
-
       var clazz;
       if (this.state.canHold) {
         clazz = "hold"
@@ -154,7 +39,7 @@ var HoldEditPanel = React.createClass({
       return <div className="hold-edit-panel">
                <h1>HOLD</h1>
                <div className={clazz} onClick={this.onSetHold}>
-                 {structure}
+                 <Notices type={this.state.type} />
                </div>
              </div>
   }
--- js/components/hold-replay.jsx
+++ js/components/hold-replay.jsx
@@ -25,121 +25,6 @@ var HoldReplayPanel = React.createClass({
   },
 
   render: function() {
-      switch (this.state.type) {
-      case C.CellType.None:
-        structure = <table className="notice-none" key="none" />
-        break;
-
-      case C.CellType.I:
-        structure =
-          <table className="notice-i" key="i">
-            <tr>
-              <td className="notice-cell type-i" />
-              <td className="notice-cell type-i" />
-              <td className="notice-cell type-i" />
-              <td className="notice-cell type-i" />
-            </tr>
-          </table>
-        break;
-
-      case C.CellType.J:
-        structure =
-          <table className="notice-j" key="j">
-            <tr>
-              <td className="notice-cell type-j" />
-              <td className="notice-cell" />
-              <td className="notice-cell" />
-            </tr>
-            <tr>
-              <td className="notice-cell type-j" />
-              <td className="notice-cell type-j" />
-              <td className="notice-cell type-j" />
-            </tr>
-          </table>
-        break;
-
-      case C.CellType.L:
-        structure =
-          <table className="notice-l" key="l">
-            <tr>
-              <td className="notice-cell" />
-              <td className="notice-cell" />
-              <td className="notice-cell type-l" />
-            </tr>
-            <tr>
-              <td className="notice-cell type-l" />
-              <td className="notice-cell type-l" />
-              <td className="notice-cell type-l" />
-            </tr>
-          </table>
-        break;
-
-      case C.CellType.O:
-        structure =
-          <table className="notice-o" key="o">
-            <tr>
-              <td className="notice-cell type-o" />
-              <td className="notice-cell type-o" />
-            </tr>
-            <tr>
-              <td className="notice-cell type-o" />
-              <td className="notice-cell type-o" />
-            </tr>
-          </table>
-        break;
-
-      case C.CellType.S:
-        structure =
-          <table className="notice-s" key="s">
-            <tr>
-              <td className="notice-cell" />
-              <td className="notice-cell type-s" />
-              <td className="notice-cell type-s" />
-            </tr>
-            <tr>
-              <td className="notice-cell type-s" />
-              <td className="notice-cell type-s" />
-              <td className="notice-cell" />
-            </tr>
-          </table>
-        break;
-
-      case C.CellType.T:
-        structure =
-          <table className="notice-t" key="t">
-            <tr>
-              <td className="notice-cell" />
-              <td className="notice-cell type-t" />
-              <td className="notice-cell" />
-            </tr>
-            <tr>
-              <td className="notice-cell type-t" />
-              <td className="notice-cell type-t" />
-              <td className="notice-cell type-t" />
-            </tr>
-          </table>
-        break;
-
-      case C.CellType.Z:
-        structure =
-          <table className="notice-z" key="z">
-            <tr>
-              <td className="notice-cell type-z" />
-              <td className="notice-cell type-z" />
-              <td className="notice-cell" />
-            </tr>
-            <tr>
-              <td className="notice-cell" />
-              <td className="notice-cell type-z" />
-              <td className="notice-cell type-z" />
-            </tr>
-          </table>
-        break;
-
-      default:
-        throw new Error('invalid type(' + this.props.type + ')');
-      }
-
       var clazz;
       if (this.state.canHold) {
         clazz = "hold"
@@ -150,7 +35,7 @@ var HoldReplayPanel = React.createClass({
       return <div className="hold-panel">
                <h1>HOLD</h1>
                <div className={clazz}>
-                 {structure}
+                 <Notices type={this.state.type} />
                </div>
              </div>
     }
